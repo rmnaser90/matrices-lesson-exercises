b42dea50d7ab7273d1d86027a8572ac9e6782074
rmnaser90 <rmnaser90@gmail.com>
2020-12-15 16:09:22 +0200

    finish all exerdises

--- src/EmployeeMatrix.js
+++ src/EmployeeMatrix.js
@@ -1,12 +1,84 @@
 /* Write your code below */
+const Matrix = require('./Matrix')
+
+class EmployeeMatrix extends Matrix {
+    constructor(rows, columns) {
+        super(0, 0)
+    }
+
+    loadData(data) {
+
+        this.matrix = data.map(d => [d._id, d.name, d.department, d.salary])
+        
+    }
+
+    getEmployees(department){
+        const employees=[]
+        for (const employee of this.matrix) {
+            if(employee[2]== department){
+                employees.push(employee[1])
+            }
+        }
+        return employees
+    }
+
+    getTotalSalary(department){
+        let totalSalaries=0
+        for (const employee of this.matrix) {
+            if(employee[2]== department){
+                totalSalaries+=employee[3]
+            }
+        }
+        return totalSalaries
+    }
+
+    findRichest(){
+        let richest=this.matrix[0]
+
+        for (const employee of this.matrix) {
+            if(employee[3]> richest[3]){
+                richest = employee
+            }
+        }
+        return richest[1]
+    }
+
+}
 
 
 
 
 //You can paste the code from the lesson below to test your solution
+let data = [
+    { _id: "e10021", name: "Gillian", department: "Finance", salary: 2000 },
+    { _id: "e10725", name: "Tibor", department: "Design", salary: 1200 },
+    { _id: "e10041", name: "Anisha", department: "Finance", salary: 2300 },
+    { _id: "e10411", name: "Jakub", department: "Design", salary: 1600 },
+    { _id: "e11995", name: "Mar", department: "Design", salary: 1300 },
+    { _id: "e10732", name: "Nisha", department: "Design", salary: 1200 }
+]
 
+let m = new EmployeeMatrix()
 
+m.loadData(data)
+// m.print()
+// m.printRow(1)
+//prints
 
+console.log(m.getEmployees("Finance")) //prints [ 'Gillian', 'Anisha' ]
+console.log(m.getEmployees("Design")) //prints [ 'Tibor', 'Jakub', 'Mar', 'Nisha' ]
+
+console.log(m.getTotalSalary("Finance")) //prints 4300
+console.log(m.getTotalSalary("Design")) //prints 5300
+
+console.log(m.findRichest()) //prints Anisha
+
+// e10021  Gillian Finance 2000
+// e10725  Tibor   Design  1200
+// e10041  Anisha  Finance 2300
+// e10411  Jakub   Design  1600
+// e11995  Mar     Design  1300
+// e10732  Nisha   Design  1200
 
 /* Do not remove the exports below */
 module.exports = EmployeeMatrix
--- src/Matrix.js
+++ src/Matrix.js
@@ -1,11 +1,88 @@
 /* Write your code below */
 
+class Matrix {
+    constructor(rows, columns) {
+        this.matrix = this._generateMatrix(rows, columns)
 
+    }
+
+    _generateMatrix(rowsNo, columnsNo) {
+        const matrix = []
+        let counter = 1
+        for (let i = 0; i < rowsNo; i++) {
+            const row = []
+            for (let j = 0; j < columnsNo; j++) {
+                row.push(counter)
+                counter++
+            }
+            matrix.push(row)
+
+        }
+        return matrix
+    }
+
+    get(row, column) {
+        return this.matrix[row][column]
+    }
+
+    alter(row, column, newValue) {
+        this.matrix[row][column] = newValue
+    }
+    print() {
+        for (let i = 0; i < this.matrix.length; i++) {
+            this.printRow(i)
+        }
+
+    }
+
+    printColumn(columnNo) {
+        for (let i = 0; i < this.matrix.length; i++) {
+            console.log(this.matrix[i][columnNo] + "\n");
+
+        }
+    }
+    printRow(rowNo) {
+        let toPrint = ""
+        for (let i = 0; i < this.matrix[0].length; i++) {
+            toPrint += this.matrix[rowNo][i] + "\t";
+        }
+        console.log(toPrint);
+
+    }
+    findCoordinate(value) {
+
+        for (let i = 0; i < this.matrix.length; i++) {
+            const row = []
+            for (let j = 0; j < this.matrix[0].length; j++) {
+                if (this.matrix[i][j]=== value) {
+                    return `x: ${j}, y: ${i}`
+                }
+            }
+        }
+
+    }
+
+
+
+}
 
 
 //You can paste the code from the lesson below to test your solution
+// let m = new Matrix(3, 4)
+// m.print()
+//prints
+/*
+1       2       3       4
+5       6       7       8
+9       10      11      12
+*/
 
+// m.alter(0, 0, m.get(1, 1))
+// m.printColumn(0) //prints 6, 5, 9 (separate lines)
+// m.printRow(0) //prints 6, 2, 3, 4 (separate lines)
 
+// console.log(m.findCoordinate(12)) //prints {x: 3, y: 2}
+// console.log(m.findCoordinate(7)) //prints {x: 2, y: 1}
 
 
 /* Do not remove the exports below */
